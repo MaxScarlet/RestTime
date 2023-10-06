4f57e7eccc017d1577ffa03657990e01e22a32ff
MaxScarlet <maxim.kulik99@gmail.com>
2023-10-06 13:22:10 +0300

    Added 404 page

--- Frontend/src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
+++ Frontend/src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
@@ -1,11 +1,9 @@
 import "./PageNotFound.css";
-import image from "../../../../public/images/404_page.jpg";
 
 function PageNotFound(): JSX.Element {
   return (
     <div className="PageNotFound">
-      <span> 404 page not found</span>
-      <img src={image} />
+      <img src="/images/404_page.jpg" />
     </div>
   );
 }
